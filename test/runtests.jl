d0030417a2ef77413bbae5b19ae20556561180d6
KwatMDPhD <kwat.me@icloud.com>
2026-05-01 05:22:36 -0600

    Deliver.

--- test/runtests.jl
+++ test/runtests.jl
@@ -56,7 +56,7 @@ Library.GR[] = const GR = Library.Graph()
 Output
 
 Library.write_graph(
-    joinpath(Barrier.P2, "graph.html"),
+    joinpath(Barrier.P2, "Graph.html"),
     GR,
     "";
     d2_ = (
@@ -75,3 +75,120 @@ Library.write_graph(
     n1 = 96,
     di = Dict("name" => "breadthfirst", "roots" => ("Output",)),
 )
+
+for (st, he, st_, N) in (
+    (
+        "Blood",
+        Library.AK,
+        [
+            "Antigen recognition",
+            "Transmigration",
+            "Hematopoiesis",
+            "The complement system",
+            "Immune memory",
+            "Humoral response",
+            "Cytotoxicity",
+            "Clonal selection",
+            "Adaptive immunity",
+            "Antigen presentation",
+            "Innate immunity",
+            "Self tolerance",
+        ],
+        [
+            1 1.4
+            1 1
+            1 1
+            1 1.1
+            1 1.2
+            1 0.9
+            1 1.1
+            1 0.9
+            1 1
+            1 1.1
+            1 1.2
+            1 1.1
+        ],
+    ),
+    (
+        "Airway",
+        Library.AO,
+        [
+            "Barrier",
+            "Stem",
+            "Circulation",
+            "Repair",
+            "Innate immunity",
+            "Adaptive immunity",
+        ],
+        [
+            1 0.8
+            1 0.9
+            1 1.1
+            1 1.3
+            1 1.4
+            1 1.1
+        ],
+    ),
+    (
+        "Gut",
+        Library.MI,
+        [
+            "Barrier",
+            "Leak",
+            "Stem",
+            "Circulation",
+            "Repair",
+            "Innate immunity",
+            "Adaptive immunity",
+            "Tolerance",
+        ],
+        [
+            1 0.8
+            1 1.4
+            1 0.9
+            1 1.1
+            1 1.3
+            1 1.4
+            1 1.1
+            1 0.7
+        ],
+    ),
+    (
+        "Skin",
+        Library.HU,
+        [
+            "Barrier",
+            "Hydration",
+            "Stem",
+            "Circulation",
+            "Repair",
+            "Innate immunity",
+            "Adaptive immunity",
+        ],
+        [
+            1 1.1
+            1 1.2
+            1 1.1
+            1 1.1
+            1 0.9
+            1 1.1
+            1 1.3
+        ],
+    ),
+)
+
+    Library.write_radar(
+        joinpath(Barrier.P2, "Immunogram $st.html"),
+        st_,
+        ["Healthy", "Sample"],
+        N,
+        ("#c6c2b6", he),
+        Dict(
+            "title" => Dict("text" => "$st Immunogram"),
+            "polar" =>
+                Dict("angularaxis" => Dict("tickfont" => Dict("size" => 32))),
+            "legend" => Dict("font" => Dict("size" => 32)),
+        ),
+    )
+
+end
